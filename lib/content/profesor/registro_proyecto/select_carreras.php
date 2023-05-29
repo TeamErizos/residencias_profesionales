8a4f22976d15baf87c4083373b089ba241eebc62
TAFFAREL11 <ataffarel2010@hotmail.com>
2023-05-29 02:16:50 -0500

    Panel Profesor Validaciones
    Se agregaron validaciones para evitar que puedas darle al boton de enviar sin antes seleccionar una opcion e los siguientes formularios:
    select_new_asesor.php
    select_carreras.php

--- lib/content/profesor/registro_proyecto/select_carreras.php
+++ lib/content/profesor/registro_proyecto/select_carreras.php
@@ -1,99 +1,119 @@
 
-<?php include("../view/header.php");?>
+    <?php include("../view/header.php");?>
 
-<!-- Condición 
-    El usuario que registra debe de seleccionar las 
-    carreras de interes para el proyecto
--->
-<h4 class="tableTitle">Registro de Proyectos</h4>
-<div class="containerFormCentered">
-<form method="post" action="select_new_asesor.php">
+    <!-- Condición 
+        El usuario que registra debe de seleccionar las 
+        carreras de interes para el proyecto
+    -->
+    <h4 class="tableTitle">Registro de Proyectos</h4>
+    <div class="containerFormCentered">
+    <form method="post" action="select_new_asesor.php" id="projectForm">
 
-<div class="input-checkbox">
+    <div class="input-checkbox">
 
-<!-- RadioButton de las carreras escogibles -->
+    <!-- RadioButton de las carreras escogibles -->
 
-</br>
+    </br>
 
-<h3>Selecciona las carreras que te interesen:</h3>
+    <h3>Selecciona las carreras que te interesen:</h3>
 
-</br>
+    </br>
 
-    <input type="checkbox" name="carrera[]" value="373436" id="IA">
-    <label for="IA">Ingeniería en Administración</label><br>
+        <input type="checkbox" name="carrera[]" value="373436" id="IA">
+        <label for="IA">Ingeniería en Administración</label><br>
 
-</br>
+    </br>
 
-    <input type="checkbox" name="carrera[]" value="407567" id="LA">
-    <label for="LA">Licenciatura en Administración</label><br>
+        <input type="checkbox" name="carrera[]" value="407567" id="LA">
+        <label for="LA">Licenciatura en Administración</label><br>
 
-</br>
+    </br>
 
-    <input type="checkbox" name="carrera[]" value="743202" id="LT">
-    <label for="LT">Licenciatura en Turismo</label><br>
+        <input type="checkbox" name="carrera[]" value="743202" id="LT">
+        <label for="LT">Licenciatura en Turismo</label><br>
 
-</br>
+    </br>
 
-    <input type="checkbox" name="carrera[]" value="902922" id="LB">
-    <label for="LB">Licenciatura en Biología</label><br>
-   
-</br>
+        <input type="checkbox" name="carrera[]" value="902922" id="LB">
+        <label for="LB">Licenciatura en Biología</label><br>
+    
+    </br>
 
-    <input type="checkbox" name="carrera[]" value="906829" id="A">
-    <label for="A">Arquitectura</label><br>
+        <input type="checkbox" name="carrera[]" value="906829" id="A">
+        <label for="A">Arquitectura</label><br>
 
-</br>
+    </br>
 
-    <input type="checkbox" name="carrera[]" value="897879" id="IC">
-    <label for="IC">Ingeniería Civil</label><br>
+        <input type="checkbox" name="carrera[]" value="897879" id="IC">
+        <label for="IC">Ingeniería Civil</label><br>
 
-</br>
+    </br>
 
-    <input type="checkbox" name="carrera[]" value="696443" id="CP">
-    <label for="CP">Contador Público</label><br>
+        <input type="checkbox" name="carrera[]" value="696443" id="CP">
+        <label for="CP">Contador Público</label><br>
 
-</br>
+    </br>
 
-    <input type="checkbox" name="carrera[]" value="297376" id="IE">
-    <label for="IE">Ingeniería Eléctrica</label><br>
+        <input type="checkbox" name="carrera[]" value="297376" id="IE">
+        <label for="IE">Ingeniería Eléctrica</label><br>
 
-</br>
+    </br>
 
-    <input type="checkbox" name="carrera[]" value="279083" id="IGE">
-    <label for="IGE">Ingeniería en Gestión Empresarial</label><br>
+        <input type="checkbox" name="carrera[]" value="279083" id="IGE">
+        <label for="IGE">Ingeniería en Gestión Empresarial</label><br>
 
-</br>
+    </br>
 
-    <input type="checkbox" name="carrera[]" value="106261" id="ISC">
-    <label for="ISC">Ingeniería en Sistemas Computacionales</label><br>
+        <input type="checkbox" name="carrera[]" value="106261" id="ISC">
+        <label for="ISC">Ingeniería en Sistemas Computacionales</label><br>
 
-</br>
+    </br>
 
-    <input type="checkbox" name="carrera[]" value="174429" id="ITIC">
-    <label for="ITIC">Ingeniería en Tecnologías de la Información y Comunicaciones</label><br>
+        <input type="checkbox" name="carrera[]" value="174429" id="ITIC">
+        <label for="ITIC">Ingeniería en Tecnologías de la Información y Comunicaciones</label><br>
 
-    <?php
+        <?php
 
-        // Establecer una cookie con el mismo nombre y un tiempo de expiración pasado
-        //setcookie('nombre_seleccionado', '', time() - 3600);
-        //setcookie('clave_profesor', '', time() - 3600);
-        // La cookie se eliminará del navegador cuando se reciba la respuesta del servidor
-        // Esto con el objetivo de limpiar el nombre y el id del profesor
+            // Establecer una cookie con el mismo nombre y un tiempo de expiración pasado
+            //setcookie('nombre_seleccionado', '', time() - 3600);
+            //setcookie('clave_profesor', '', time() - 3600);
+            // La cookie se eliminará del navegador cuando se reciba la respuesta del servidor
+            // Esto con el objetivo de limpiar el nombre y el id del profesor
 
 
-    ?>
+        ?>
 
-    <div class="button-container">
-        <button type="submit" value="Enviar">Enviar</button>
+        <div class="button-container">
+            <button type="submit" value="Enviar" id="submitButton">Enviar</button>
+        </div>
+
+    </br>
+
+    </div>    
+
+    <!-- Después de seleccionar, guardar las carreras seleccionadas como cookies -->
+    <!-- Script para asegurar que el usuario seleccione al menos una carrera -->
+    <script>
+    window.addEventListener('DOMContentLoaded', function() {
+        // Obtener el formulario y el botón de enviar
+        var form = document.getElementById('projectForm');
+        var submitButton = document.getElementById('submitButton');
+
+        // Agregar evento de escucha al enviar el formulario
+        form.addEventListener('submit', function(event) {
+        // Obtener todas las opciones de checkbox seleccionadas
+        var checkboxes = form.querySelectorAll('input[type="checkbox"]:checked');
+
+        // Verificar si se ha seleccionado al menos una opción
+        if (checkboxes.length === 0) {
+            // Prevenir el envío del formulario si no se ha seleccionado ninguna opción
+            event.preventDefault();
+            alert('Debes seleccionar al menos una carrera.');
+        }
+        });
+    });
+    </script>
+    </form>
     </div>
 
-</br>
-
-</div>    
-
-<!-- Después de seleccionar, guardar las carreras seleccionadas como cookies -->
-
-</form>
-</div>
-
-<?php include("../view/footer.php");?>
+    <?php include("../view/footer.php");?>
--- lib/content/profesor/registro_proyecto/select_new_asesor.php
+++ lib/content/profesor/registro_proyecto/select_new_asesor.php
@@ -1,20 +1,14 @@
 <!-- Condición:
-
     Se deben mostrar los profesores de las carreras de interés seleccionadas
     Debe de contar con una función de regresar
-
 -->
 
 <?php
-
 include("../view/header.php");
 
 // Archivos requeridos
 require "../../../login/conexion/conectAWS.php";
 require "resources/funciones_proyecto.php";
-
-
-
 
 // Verificar si se hizo un envío de formulario
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
@@ -30,55 +24,84 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         // Obtener los nombres de los profesores de la carrera seleccionada
         $nombres_profesores = $proyecto->buscarProfesoresPorCarrera($carreras_ids);
 
-        // -----------------------------------------------------------------------------
+        // Verificar si se encontraron profesores
+        if (!empty($nombres_profesores)) {
+            // -----------------------------------------------------------------------------
 
-        // Obtener los nombres de las carreras seleccionadas y guardarlas en cookies
-        // $nombres_carreras = $proyecto->obtenerNombreCarrera($carreras_ids);
+            // Obtener los nombres de las carreras seleccionadas y guardarlas en cookies
+            // $nombres_carreras = $proyecto->obtenerNombreCarrera($carreras_ids);
 
-        // Serializar el array de nombres de carreras
-        $nombres_carreras_serializados = serialize($carreras_ids);
+            // Serializar el array de nombres de carreras
+            $nombres_carreras_serializados = serialize($carreras_ids);
 
-        // Establecer el tiempo de vida de la cookie (en este caso, 30 días)
-        $tiempo_de_vida = time() + (30 * 24 * 60 * 60);
+            // Establecer el tiempo de vida de la cookie (en este caso, 30 días)
+            $tiempo_de_vida = time() + (30 * 24 * 60 * 60);
 
-        // Guardar el array de nombres de carreras serializado en una cookie
-        setcookie("id_carreras", $nombres_carreras_serializados, $tiempo_de_vida);
-
-?>
-        <h4 class="centeredTitle">Seleccione el nuevo Asesor</h4>
-        <div class="containerFormCentered">
-            <div class="centered-div">
-                <form class="formContainerCheckbox" action="form_proyecto.php" method="post">
-                    <h3>Asesores:</h3>
-
-
-            <?php   // Recibo un array con nombres, de ese array debo generar
-            // Radio buttons por cada nombre recibido
-
-            // Recorrer cada fila de resultados
-            foreach ($nombres_profesores as $fila) {
-
-                // Almacenar el id_profesor y el nombre completo en variables
-                $id_profesor = $fila['id_profesor'];
-                $nombre_completo = $fila['nombre_completo'];
-
-                // Imprimir el radiobutton con el id_profesor como value y el nombre completo como texto
-                echo '<div class="container_btn">';
-                echo '<label class="custom-label">';
-                echo "<input type='radio' name='nombre' value='$id_profesor'/>";
-                echo "<span>$nombre_completo</span>";
-                echo '</label>';
-                echo '</div>';
-            }
+            // Guardar el array de nombres de carreras serializado en una cookie
+            setcookie("id_carreras", $nombres_carreras_serializados, $tiempo_de_vida);
+        } else {
+            // Mostrar una alerta si no se encontraron profesores
+            echo '<script>alert("No se encontraron profesores para las carreras seleccionadas.");</script>';
+            exit; // Detener la ejecución del código
         }
+    } else {
+        // Mostrar una alerta si no se seleccionaron carreras
+        echo '<script>alert("Debes seleccionar al menos una carrera.");</script>';
+        exit; // Detener la ejecución del código
     }
+}
+?>
 
-            ?>
-            <div class="button-containerForm">
-                <input type="submit" value="Enviar">
-            </div>
-                </form>
-            </div>
-        </div>
+<h4 class="centeredTitle">Seleccione el nuevo Asesor</h4>
+<div class="containerFormCentered">
+  <div class="centered-div">
+    <form class="formContainerCheckbox" action="form_proyecto.php" method="post" id="asesorForm">
+      <h3>Asesores:</h3>
 
-        <?php include("../view/footer.php"); ?>
+      <?php
+      // Verificar si se encontraron profesores
+      if (!empty($nombres_profesores)) {
+        // Recorrer cada fila de resultados
+        foreach ($nombres_profesores as $fila) {
+          // Almacenar el id_profesor y el nombre completo en variables
+          $id_profesor = $fila['id_profesor'];
+          $nombre_completo = $fila['nombre_completo'];
+
+          // Imprimir el radiobutton con el id_profesor como value y el nombre completo como texto
+          echo '<div class="container_btn">';
+          echo '<label class="custom-label">';
+          echo "<input type='radio' name='nombre' value='$id_profesor'/>";
+          echo "<span>$nombre_completo</span>";
+          echo '</label>';
+          echo '</div>';
+        }
+      }
+      ?>
+
+      <div class="button-containerForm">
+        <input type="submit" value="Enviar">
+      </div>
+    </form>
+  </div>
+</div>
+
+<script>
+  // Obtener el formulario y el botón de enviar
+  var form = document.getElementById('asesorForm');
+  var submitButton = document.querySelector('input[type="submit"]');
+
+  // Agregar evento de escucha al enviar el formulario
+  form.addEventListener('submit', function(event) {
+    // Obtener el valor del radio button seleccionado
+    var selectedAsesor = form.querySelector('input[type="radio"]:checked');
+
+    // Verificar si se ha seleccionado un asesor
+    if (!selectedAsesor) {
+      // Prevenir el envío del formulario si no se ha seleccionado un asesor
+      event.preventDefault();
+      alert('Debes seleccionar al menos un asesor.');
+    }
+  });
+</script>
+
+<?php include("../view/footer.php"); ?>
