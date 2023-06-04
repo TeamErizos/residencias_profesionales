5457502112ba0be2b1361a702eb2e3d3aa0abf7c
Virsen <kamellaso@hotmail.com>
2023-06-04 05:56:39 +0200

    Solicitud_Residencia
    Cambios en solicitud residencia.php
    FUNCIONAL

--- lib/content/alumno/solicitud_residencia/solicitud_residencia.php
+++ lib/content/alumno/solicitud_residencia/solicitud_residencia.php
@@ -94,54 +94,79 @@ $alumno->recuperarAsesorExterno($_SESSION['id_proyecto']);
 ?>
 
 
-<html>
 
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device=width, initial-scale=1.0">
-  <link rel="stylesheet" href="style_banco_solicitud.css">
-  <title>Banco de Proyectos</title>
-</head>
+<body>
+<link rel="stylesheet" href="/residencias_profesionales/lib/content/alumno/view/formatosSolicitud2.css">
 
-<div class="containerForm">
+
+
+<title>Banco de Proyectos</title>
+
+
+
+
+
+<div class="containerForm"  >
 
 <form action="resources/guardar_solicitud.php" method="post" enctype="multipart/form-data">
   <!-- Datos del Proyecto -->
-  <div>
-    <h3>Datos del Proyecto</h3>
 
+
+
+
+
+
+   
+  <div class="content">
+    <!-- Datos del Proyecto -->
+    <h3 class="input-box full-width">Datos del Proyecto</h3>
+ 
+    </br>
+     <!-- Contenedor columnas -->
+    <div class="column-container">
+  <!-- Columna izquierda -->
+  <div class="column">
+    
     <!-- Nombre del proyecto -->
-    <div>
+    <div class="input-box full-width">
+
       <label for="project_name">Nombre del proyecto:</label>
+
+     
       <!-- Este no es variable de sesión -->
       <input type="text" value="<?php echo $_SESSION['nombre_proyecto']; ?>" id="project_name" name="project_name"
         disabled>
     </div>
 
+
     <!-- Tipo de Proyecto -->
-    <div>
+    <div class="input-box">
       <label for="project_type">Tipo de Proyecto:</label>
       <input type="text" value="<?php echo $_SESSION['tipo_proyecto']; ?>" id="project_type" name="project_type"
         disabled>
     </div>
 
     <!-- Opcion elegida -->
-    <div>
+    <div class="input-box">
       <label for="selected_option">Opcion elegida:</label>
       <input type="text" value="<?php echo $_SESSION['origen_proyecto']; ?>" id="selected_option" name="selected_option"
         disabled>
     </div>
+     <!-- Fin Columna1 -->
+    </div>
 
+
+     <!-- Columna2 -->
+     <div class="column">
     <!-- Periodo proyectado -->
-    <div>
+    <div class="input-box">
       <label for="projected_period">Periodo proyectado:</label>
       <input type="text" value="<?php echo ($alumno->getPeriodo()); ?>" id="projected_period" name="projected_period"
         disabled>
     </div>
 
     <!-- Nombre del asesor interno -->
-    <div>
+    <div class="input-box">
       <!-- esta no es variable de sesión -->
       <label for="internal_advisor">Nombre del asesor interno:</label>
       <input type="text" value="<?php echo $_SESSION['nombre_profe']; ?>" id="internal_advisor" name="internal_advisor"
@@ -149,206 +174,48 @@ $alumno->recuperarAsesorExterno($_SESSION['id_proyecto']);
     </div>
 
     <!-- Numero de residentes -->
-    <div>
+    <div class="input-box">
       <label for="num_residents">Numero de residentes:</label>
       <input type="text" value="<?php echo $_SESSION['num_residentes']; ?>" id="num_residents" name="num_residents"
         disabled>
     </div>
+     <!-- Fin Columna2 -->
+     </div>
+ <!-- Fin column-container -->
+     </div>
+
+    <!-- Fin column-content --> 
   </div>
+  </br>
 
 
 
+
+
+  <div class="content">
   <!-- Datos de la empresa -->
   <div>
-    <h3>Datos de la empresa</h3>
+    <h3 class="input-box full-width">Datos de la empresa</h3>
+    </br>
+     <!-- Contenedor columnas -->
+     <div class="column-container">
+
+  <!-- Columna izquierda -->
+  <div class="column">
+
     <!-- Nombre de la empresa -->
-    <div>
+    <div class="input-box">
       <label for="company_name">Nombre de la empresa:</label>
       <input type="text" value="<?php echo $_SESSION['nombre_empresa']; ?>" id="company_name" name="company_name"
         disabled>
     </div>
 
+
     <!-- Ramo -->
-    <div>
+    <div class="input-box">
       <label for="branch">Ramo:</label>
       <input type="text" value="<?php echo $_SESSION['ramo_empresa']; ?>" id="branch" name="branch" disabled>
     </div>
-
-    <!-- RFC -->
-    <div>
-      <label for="rfc">RFC:</label>
-      <input type="text" value="<?php echo $_SESSION['rfc_empresa']; ?>" id="rfc" name="rfc" disabled>
-    </div>
-
-    <!-- Sector -->
-    <div>
-      <label for="sector">Sector:</label>
-      <input type="text" value="<?php echo $_SESSION['sector_empresa']; ?>" id="sector" name="sector" disabled>
-    </div>
-
-    <!-- Actividad principal -->
-    <div>
-      <label for="main_activity">Actividad principal:</label>
-      <input type="text" value="<?php echo $_SESSION['sector_empresa']; ?>" id="main_activity" name="main_activity"
-        disabled>
-    </div>
-
-    <!-- Domicilio -->
-    <div>
-      <label for="address">Domicilio:</label>
-      <input type="text" value="<?php echo $_SESSION['domicilio_empresa']; ?>" id="address" name="address" disabled>
-    </div>
-
-    <!-- Colonia -->
-    <div>
-      <label for="colony">Colonia:</label>
-      <input type="text" value="<?php echo $_SESSION['colonia_empresa']; ?>" id="colony" name="colony" disabled>
-    </div>
-
-    <!-- Codigo Postal -->
-    <div>
-      <label for="cp">CP:</label>
-      <input type="text" value="<?php echo $_SESSION['codigo_postal']; ?>" id="cp" name="cp" disabled>
-    </div>
-
-    <!-- FAX -->
-    <div>
-      <label for="fax">Fax:</label>
-      <input type="text" value="<?php echo $_SESSION['fax_empresa']; ?>" id="fax" name="fax" disabled>
-    </div>
-
-    <!-- Ciudad -->
-    <div>
-      <label for="city">Ciudad:</label>
-      <input type="text" value="<?php echo $_SESSION['ciudad_empresa']; ?>" id="city" name="city" disabled>
-    </div>
-
-    <!-- Telefono -->
-    <div>
-      <label for="phone">Telefono:</label>
-      <input type="text" value="<?php echo $_SESSION['tel_empresa']; ?>" id="phone" name="phone" disabled>
-    </div>
-
-    <!-- Nombre del titular de la empresa -->
-    <div>
-      <label for="company_holder">Nombre del titular de la empresa:</label>
-      <input type="text" value="<?php echo $_SESSION['nombre_del_titular_de_empresa']; ?>" id="company_holder"
-        name="company_holder" disabled>
-    </div>
-
-    <!-- Puesto -->
-    <div>
-      <label for="position">Puesto:</label>
-      <input type="text" value="<?php echo $_SESSION['puesto_titular']; ?>" id="position" name="position" disabled>
-    </div>
-  </div>
-
-  <div id="required_data">
-    <!-- Nombre del asesor externo -->
-    <div>
-      <label for="external_advisor">Nombre del asesor externo:</label>
-      <input type="text" id="external_advisor" name="external_advisor"
-        value="<?php echo $_SESSION['nombre_asesor_externo']; ?>" disabled>
-    </div>
-
-    <!-- Puesto del asesor externo -->
-    <div>
-      <label for="external_advisor_position">Puesto del asesor externo:</label>
-      <input type="text" id="external_advisor_position" name="external_advisor_position"
-        value="<?php echo $_SESSION['puesto_asesor_externo']; ?>" disabled>
-    </div>
-
-    <!-- Nombre de la persona que firmará el acuerdo -->
-    <div>
-      <label for="agreement_signer">Nombre de la persona que firmará el acuerdo:</label>
-      <input type="text" id="agreement_signer" name="agreement_signer">
-    </div>
-
-    <!-- Puesto de la persona que firmará el acuerdo -->
-    <div>
-      <label for="agreement_signer_position">Puesto de la persona que firmará el acuerdo:</label>
-      <input type="text" id="agreement_signer_position" name="agreement_signer_position">
-    </div>
-
-
-  </div>
-
-
-  <!-- Datos del Alumno -->
-  <div>
-    <h3>Datos del Alumno</h3>
-    <!-- Nombre del alumno -->
-    <div>
-      <label for="student_name">Nombre del alumno:</label>
-      <input type="text" value="<?php echo $_SESSION['nombre_alumno']; ?>" id="student_name" name="student_name"
-        disabled>
-    </div>
-
-    <!-- Carrera -->
-    <div>
-      <label for="career">Carrera:</label>
-      <input type="text" value="<?php echo $_SESSION['nombre_carrera']; ?>" id="career" name="career" disabled>
-    </div>
-
-    <!-- No. de control -->
-    <div>
-      <label for="control_num">Numero de control:</label>
-      <input type="text" value="<?php echo $_SESSION['no_control']; ?>" id="control_num" name="control_num" disabled>
-    </div>
-
-    <!-- Domicilio -->
-    <div>
-      <label for="student_address">Domicilio:</label>
-      <input type="text" value="<?php echo $_SESSION['domicilio_alumno']; ?>" id="student_address" name="student_address"
-        disabled>
-    </div>
-
-    <!-- Email -->
-    <div>
-      <label for="student_email">Email:</label>
-      <input type="email" value="<?php echo $_SESSION['correo_alumno']; ?>" id="student_email" name="student_email"
-        disabled>
-    </div>
-
-    <!-- Ciudad -->
-    <div>
-      <label for="student_city">Ciudad:</label>
-      <input type="text" value="<?php echo $_SESSION['ciudad_alumno']; ?>" id="student_city" name="student_city"
-        disabled>
-    </div>
-
-    <!-- Telefono -->
-    <div>
-      <label for="student_phone">Telefono:</label>
-      <input type="text" value="<?php echo $_SESSION['telefono_alumno']; ?>" id="student_phone" name="student_phone"
-        disabled>
-    </div>
-
-    <!-- Tipo de seguro -->
-    <div>
-      <label for="insurance_type">Tipo de seguro:</label>
-      <input type="text" value="<?php echo $_SESSION['seguro_medico_alumno']; ?>" id="insurance_type"
-        name="insurance_type" disabled>
-    </div>
-
-    <!-- Numero de seguro social -->
-    <div>
-      <label for="social_security_num">Numero de seguro social:</label>
-      <input type="text" value="<?php echo $_SESSION['num_seguridad_social_alumno']; ?>" id="social_security_num"
-        name="social_security_num" disabled>
-    </div>
-
-    <!-- Semestre a cursar -->
-    <div>
-      <label for="semester">Semestre a cursar:</label>
-      <input type="text" id="semester" name="semester">
-    </div>
-
-    <!-- Espacios para cargar envios de archivos -->
-    <!-- Cargar Anteproyecto -->
-    <input type="file" name="anteproyecto">
-    <!-- Cargar Constancia de Residencia -->
-    <input type="file" name="constancia">
 
     <!-- RFC -->
     <div class="input-box">
@@ -356,14 +223,221 @@ $alumno->recuperarAsesorExterno($_SESSION['id_proyecto']);
       <input type="text" value="<?php echo $_SESSION['rfc_empresa']; ?>" id="rfc" name="rfc" disabled>
     </div>
 
+    <!-- Sector -->
+    <div class="input-box">
+      <label for="sector">Sector:</label>
+      <input type="text" value="<?php echo $_SESSION['sector_empresa']; ?>" id="sector" name="sector" disabled>
+    </div>
+
+    <!-- Actividad principal -->
+    <div class="input-box">
+      <label for="main_activity">Actividad principal:</label>
+      <input type="text" value="<?php echo $_SESSION['sector_empresa']; ?>" id="main_activity" name="main_activity"
+        disabled>
+    </div>
+
+    <!-- Domicilio -->
+    <div class="input-box">
+      <label for="address">Domicilio:</label>
+      <input type="text" value="<?php echo $_SESSION['domicilio_empresa']; ?>" id="address" name="address" disabled>
+    </div>
+
+    <!-- Colonia -->
+    <div class="input-box">
+      <label for="colony">Colonia:</label>
+      <input type="text" value="<?php echo $_SESSION['colonia_empresa']; ?>" id="colony" name="colony" disabled>
+    </div>
+<!-- Fin Columna1 -->
+</div>
+
+ <!-- Columna2 -->
+ <div class="column">
+
+    <!-- Codigo Postal -->
+    <div class="input-box">
+      <label for="cp">CP:</label>
+      <input type="text" value="<?php echo $_SESSION['codigo_postal']; ?>" id="cp" name="cp" disabled>
+    </div>
+
+    <!-- FAX -->
+    <div class="input-box">
+      <label for="fax">Fax:</label>
+      <input type="text" value="<?php echo $_SESSION['fax_empresa']; ?>" id="fax" name="fax" disabled>
+    </div>
+
+
+
+    <!-- Ciudad -->
+    <div class="input-box">
+      <label for="city">Ciudad:</label>
+      <input type="text" value="<?php echo $_SESSION['ciudad_empresa']; ?>" id="city" name="city" disabled>
+    </div>
+
+    <!-- Telefono -->
+    <div class="input-box">
+      <label for="phone">Telefono:</label>
+      <input type="text" value="<?php echo $_SESSION['tel_empresa']; ?>" id="phone" name="phone" disabled>
+    </div>
+
+    <!-- Nombre del titular de la empresa -->
+    <div class="input-box">
+      <label for="company_holder">Nombre del titular de la empresa:</label>
+      <input type="text" value="<?php echo $_SESSION['nombre_del_titular_de_empresa']; ?>" id="company_holder"
+        name="company_holder" disabled>
+    </div>
+
+    <!-- Puesto -->
+    <div class="input-box">
+      <label for="position">Puesto:</label>
+      <input type="text" value="<?php echo $_SESSION['puesto_titular']; ?>" id="position" name="position" disabled>
+    </div>
   </div>
 
+ 
+
+<!-- Fin Columna2 -->
+</div>
+
+
+
+<div id="required_data">
+    <!-- Nombre del asesor externo -->
+    <div class="input-box">
+      <label for="external_advisor">Nombre del asesor externo:</label>
+      <input type="text" id="external_advisor" name="external_advisor"
+        value="<?php echo $_SESSION['nombre_asesor_externo']; ?>" disabled>
+    </div>
+
+  
+
+    <!-- Puesto del asesor externo -->
+    <div class="input-box">
+      <label for="external_advisor_position">Puesto del asesor externo:</label>
+      <input type="text" id="external_advisor_position" name="external_advisor_position"
+        value="<?php echo $_SESSION['puesto_asesor_externo']; ?>" disabled>
+    </div>
+
+    
+ 
+    <!-- Nombre de la persona que firmará el acuerdo -->
+    <div class="input-box">
+      <label for="agreement_signer">Nombre de la persona que firmará el acuerdo:</label>
+      <input type="text" id="agreement_signer" name="agreement_signer">
+    </div>
+
+    <!-- Puesto de la persona que firmará el acuerdo -->
+    <div class="input-box">
+      <label for="agreement_signer_position">Puesto de la persona que firmará el acuerdo:</label>
+      <input type="text" id="agreement_signer_position" name="agreement_signer_position">
+    </div>
+  </div>
+
+<!-- Fin column-container -->
+</div>
+
+ 
+
+
+    <!-- Fin column-content --> 
+  </div>
+  </br>
+
+
+
+
+
+
+ 
+ 
+  <!-- Datos del Alumno -->
+  <div class="content">
+    <h3 class="input-box full-width">Datos del Alumno</h3>
+    <br>
+    <!-- Contenedor columnas -->
+    <div class="column-container">
+      <!-- Columna izquierda -->
+      <div class="column">
+        <!-- Nombre del alumno -->
+        <div class="input-box">
+          <label for="student_name">Nombre del alumno:</label>
+          <input type="text" value="<?php echo $_SESSION['nombre_alumno']; ?>" id="student_name" name="student_name" disabled>
+        </div>
+        <!-- Carrera -->
+        <div class="input-box">
+          <label for="career">Carrera:</label>
+          <input type="text" value="<?php echo $_SESSION['nombre_carrera']; ?>" id="career" name="career" disabled>
+        </div>
+        <!-- No. de control -->
+        <div class="input-box">
+          <label for="control_num">Numero de control:</label>
+          <input type="text" value="<?php echo $_SESSION['no_control']; ?>" id="control_num" name="control_num" disabled>
+        </div>
+        <!-- Domicilio -->
+        <div class="input-box">
+          <label for="student_address">Domicilio:</label>
+          <input type="text" value="<?php echo $_SESSION['domicilio_alumno']; ?>" id="student_address" name="student_address" disabled>
+        </div>
+
+      <!-- Fin Columna1 -->
+    </div>
+
+      <!-- Columna2 -->
+      <div class="column">
+
+        <!-- Email -->
+        <div class="input-box">
+          <label for="student_email">Email:</label>
+          <input type="email" value="<?php echo $_SESSION['correo_alumno']; ?>" id="student_email" name="student_email" disabled>
+        </div>
+        <!-- Ciudad -->
+        <div class="input-box">
+          <label for="student_city">Ciudad:</label>
+          <input type="text" value="<?php echo $_SESSION['ciudad_alumno']; ?>" id="student_city" name="student_city" disabled>
+        </div>
+        <!-- Telefono -->
+        <div class="input-box">
+          <label for="student_phone">Telefono:</label>
+          <input type="text" value="<?php echo $_SESSION['telefono_alumno']; ?>" id="student_phone" name="student_phone" disabled>
+        </div>
+        <!-- Tipo de seguro -->
+        <div class="input-box">
+          <label for="insurance_type">Tipo de seguro:</label>
+          <input type="text" value="<?php echo $_SESSION['seguro_medico_alumno']; ?>" id="insurance_type" name="insurance_type" disabled>
+        </div>
+       <!-- Fin Columna2 -->
+    </div>
+<!-- Fin column-container -->
+    </div>
+    <!-- Fin content -->
+  </div>
+
+  
+  <!-- Semestre a cursar -->
+  <div class="input-box">
+    <label for="semester">Semestre a cursar:</label>
+    <input type="text" id="semester" name="semester">
+  </div>
+  <!-- Espacios para cargar envios de archivos -->
+  <!-- Cargar Anteproyecto -->
+  <h3 class="input-box full-width">Cargar anteproyecto</h3>
+  <input type="file" name="anteproyecto" class="input-box full-width">
+  <!-- Cargar Constancia de Residencia -->
+  <h3 class="input-box full-width">Cargar constancia de residencia</h3>
+  <input type="file" name="constancia"  class="input-box full-width">
+  <br>
   <!-- Submit button -->
   <div>
-    <input type="submit" value="Submit">
+    <input type="submit" value="Submit" class="enviar" >
   </div>
-</form>
+
+<br>
+<!-- Fin Container Form -->
 </div>
+
+
+</body>
+
+ 
 
 
 <?php include("../view/footer.php"); ?>
